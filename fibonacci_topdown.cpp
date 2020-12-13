b63f11ed156f641b8ca2cddec51a6d8dc7d0f21b
Atreya <atreyamaj@gmail.com>
2020-12-13 15:31:33 +0530

    Added dp to Fib

--- fibonacci_topdown.cpp
+++ fibonacci_topdown.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 using namespace std;
 
-// Fibonacci using Top-Down Approach
+// Fibonacci till nth number using Top-Down Approach
 
-int fib(int n){
+int fib(int n){ //recursion
+    
     if( n == 1 or n == 0) return n;
     int ans = fib(n - 1) + fib(n-2);
     return ans;
 }
+
+int fib_dp(int n, int dp[]){
+    if (n == 0 or n == 1) return n;
+
+    //Look up
+    if(dp[n] != 0){
+        return dp[n];
+    }
+
+    int ans;
+    ans = fib_dp(n-1, dp) + fib_dp(n-2, dp);
+    return dp[n] = ans;
+}
+
 int main(){
-    int x = fib(6);
-    cout<< "Henlo";
-    cout << x;
+    int n;
+    
+    cout<<"Enter N: "<<endl;
+    int dp [100] = {0};
+    cin>>n;
+    cout<<fib_dp(n, dp)<<endl;
 }
